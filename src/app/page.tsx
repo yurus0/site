97a94d7f321e7f7e41285e975e420c3eb7c3eafc
yurus0 <imanebenrazzouk0302@gmail.com>
2024-08-29 18:03:43 +0100

    feat:structuring components and theme switching;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,135 +1,191 @@
 "use client";
 import VerticalBanner from "@/components/VerticalBanner";
+import { LockClosedIcon } from "@heroicons/react/24/outline";
+import Image from 'next/image';
 import { useEffect, useState } from "react";
 
 export default function Home() {
   let bg = "bg-black text-white";
   const [theme, setTheme] = useState<'dark' | 'light'>('dark');
   const [loaderVisible, setLoaderVisible] = useState(true);
+  const [sectionName, setSection] = useState<'exp' | 'proj' | 'cred' | 'nan'>('nan');
 
 
   useEffect(() => {
     const handleScroll = () => {
+      const section2 = document.getElementById('dark-section');
+      if (section2) {
+        const rect = section2.getBoundingClientRect();
+        const isInView = rect.top < window.innerHeight && rect.bottom > 0;
+        if (isInView) {
+          setTheme('dark');
+          setLoaderVisible(true);
+          console.log('dark');
+        }
+      }
+
       const section = document.getElementById('light-section');
       if (section) {
         const rect = section.getBoundingClientRect();
-        const isInView1 = rect.top < window.innerHeight && rect.bottom > 0;
-        if (isInView1) {
+        const isInView = rect.top < window.innerHeight && rect.bottom >= 0;
+        if (isInView) {
           setTheme('light');
           setLoaderVisible(false);
           console.log('light');
         }
-        const section2 = document.getElementById('dark-section');
-        if (section2) {
-          const rect = section2.getBoundingClientRect();
-          const isInView = rect.top < window.innerHeight && rect.bottom > 0;
-          if (isInView && !isInView1) {
-            setTheme('dark');
-            setLoaderVisible(true);
-            console.log('dark');
-          }
+      }
+      // Check if 'dark-cov-bg' is in view
+      const sectionCov = document.getElementById('dark-cov-bg');
+      if (sectionCov) {
+        const rect = sectionCov.getBoundingClientRect();
+        const isInView = rect.top < window.innerHeight && rect.top > 0;
+        if (isInView) {
+          setTheme('dark');
+        } else {
+          setTheme('light');
         }
       }
-    };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+      const sectionExp = document.getElementById('sec-exp');
+      if (sectionExp) {
+        const rect = sectionExp.getBoundingClientRect();
+        const isInView = rect.top < window.innerHeight && rect.bottom >= 0;
+        if (isInView) {
+          setSection('exp');
+          console.log('Section: exp');
+        } else {
+          setSection('nan');
+        }
+      }
+
+      // Check if 'sec-proj' is in view
+      const sectionProj = document.getElementById('sec-proj');
+      if (sectionProj) {
+        const rect = sectionProj.getBoundingClientRect();
+        const isInView = rect.top < window.innerHeight && rect.bottom >= 0;
+        if (isInView) {
+          setSection('proj');
+          console.log('Section: proj');
+        } else if (sectionName === 'proj') {
+          setSection('nan');
+        }
+      }
+      };
+
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
   }, []);
 
   return (
     <main className={`flex min-h-screen flex-col ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
       <div className={`flex flex-row ${theme === 'dark' ? 'bg-black text-white' : 'bg-white text-black'}`}>
-      <VerticalBanner theme={theme} loaderVisible={loaderVisible}/>
+      <VerticalBanner theme={theme} loaderVisible={loaderVisible} section={sectionName} />
 
       <div id='dark-section' className="flex min-h-screen flex-col p-24 items-start">
-      <div className="min-h-screen py-24 px-40">
+      <div className="min-h-min pt-24 pb-96 px-40">
         <div className="text-left">
           <div className="text-[#ffffff]/50 font-bold pb-8">Imane Benrazzouk</div>
           <div className="text-3xl font-bold pb-2">Hello, there!</div>
-          <div className="text-6xl font-bold pb-2">Hello, t<span className="text-yellow-500">h</span>er<span className="text-purple-500">e</span>!</div>
-          <div className="text-9xl font-bold pb-4">He<span className="text-cyan-500">l</span>lo, th<span className="text-pink-500">e</span>re!</div>
+          <div className="text-6xl font-bold pb-2">Hello, t<span className="hover:text-yellow-500">h</span>er<span className="hover:text-purple-500">e</span>!</div>
+          <div className="text-9xl font-bold pb-4">He<span className="hover:text-cyan-500">l</span>lo, th<span className="hover:text-pink-500">e</span>re!</div>
           <div className="text-3xl font-medium">Just a Software Engineer who likes writing shy code.</div>
         </div>
       </div>
       </div>
       </div>
-      <div className="min-h-96 flex flex-row justify-center items-center" >
-      <div id='light-section' className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://github.com/yurus0/Covmodel"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Covmodel{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-        </div>
+      <div id='sec-exp'>
+      <div className="min-h-96 pt-48" >
+      <div className=" pl-48 flex flex-col pb-20">
+        <h1 className="text-xl font-light text-black pb-4">2024</h1>
+        <h2 className="pb-2 text-4xl font-semibold text-black">
+          Bank Al Maghrib
+        </h2>
+        <p className={`text-4xl font-semibold  ${theme === 'dark' ? 'text-purple-100 opacity-40' : 'text-purple-950 opacity-20'}`}>
+          Central Bank of Morocco
+        </p>
       </div>
-      <div className="min-h-96 bg-pink-200 flex flex-row justify-center items-center">
-        <div className="text-center text-4xl">
-          Get in touch !
+      <div id='light-section' className="min-h-96 bg-purple-50">
+        <div className="flex justify-end items-center opacity-50 py-20">
+        <Image
+            src="/Bank_Al-Maghrib_logo.svg"
+            alt="bam"
+            width={500}
+            height={500}
+            className="pr-10"
+          />
+          <Image
+            src="/bkam.png"
+            alt="bam"
+            width={800}
+            height={500}
+          />
+          </div>
+      </div>
+      </div>
+      <div className="min-h-96 bg-zinc-100 flex flex-row justify-center items-center px-10 pt-10 pb-60 text-black ">
+        <div className="w-2/3 px-20 py-10 text-xl font-light">
+        For the past summer, I've been collaborating with the Data Analytics Department on an internal tool for anomaly flagging. This project involved developing and optimizing algorithms to enhance data quality and accuracy across the organization.
+        <br/> <br/>
+        The nature of the work is confidential, but if you want to learn more about this experience, feel free to reach out.
         </div>
+        <div className="w-1/4 pr-20 py-10">
+          <div className="font-bold text-xs tracking-[.15em]">
+            SWE & RESEARCH ASSISTANT
+          </div>
+          <div className="text-md font-light pt-1">
+            Data Science <br/> Machine Learning <br/> Software Development
+          </div><div className="pt-10 font-bold text-xs tracking-[.15em]">
+            LINK
+          </div>
+          <div className="text-md font-light flex flex-row items-center pt-1">
+
+            <LockClosedIcon className="h-4 w-4" />
+            <span className="pl-1 underline underline-offset-1">View Work</span>
+          </div></div>
+      </div>
+      </div>
+      <div id='sec-proj'>
+      <div className="min-h-96 pl-48 flex flex-col justify-end items-start bg-white" >
+      <div className="pb-10 flex flex-col">
+        <h2 className="pb-4 text-4xl font-semibold text-black">
+          Vowels Island{" "}
+        </h2>
+        <p className="text-4xl font-semibold opacity-50 text-blue-700">
+          Vowels game for kids with Dyslexia.
+        </p>
+      </div>
+      </div>
+      <div id='dark-cov-bg' className="min-h-96 bg-gray-950 flex flex-row justify-center items-center">
+      <Image
+            src="/cov3.png"
+            alt="covmodel"
+            width={1000}
+            height={500}
+          />
+      </div>
+      <div className="min-h-96 bg-zinc-100 flex flex-row justify-center items-center px-10 pt-10 pb-60 text-black ">
+        <div className="w-2/3 px-20 py-10 text-xl font-light">
+        For the past summer, I've been collaborating with the Data Analytics Department on an internal tool for anomaly flagging. This project involved developing and optimizing algorithms to enhance data quality and accuracy across the organization.
+        <br/> <br/>
+        The nature of the work is confidential, but if you want to learn more about this experience, feel free to reach out.
+        </div>
+        <div className="w-1/4 pr-20 py-10">
+          <div className="font-bold text-xs tracking-[.15em]">
+            SWE & RESEARCH ASSISTANT
+          </div>
+          <div className="text-md font-light pt-1">
+            Data Science <br/> Machine Learning <br/> Software Development
+          </div><div className="pt-10 font-bold text-xs tracking-[.15em]">
+            LINK
+          </div>
+          <div className="text-md font-light flex flex-row items-center pt-1">
+
+            <LockClosedIcon className="h-4 w-4" />
+            <span className="pl-1 underline underline-offset-1">View Work</span>
+          </div></div>
+      </div>
       </div>
     </main>
   );
--- src/components/VerticalBanner.tsx
+++ src/components/VerticalBanner.tsx
@@ -2,7 +2,7 @@ import { EnvelopeIcon } from "@heroicons/react/24/solid";
 import GithubIcon from "./GithubIcon";
 import VerticalLoader from "./VerticalLoader";
 
-const VerticalBanner =({theme, loaderVisible}:{theme:string; loaderVisible:boolean;})=>{
+const VerticalBanner =({theme, loaderVisible, section}:{theme:string; loaderVisible:boolean; section?:string;})=>{
     return(
         <div className="fixed z-index flex flex-col w-20 text-center items-center justify-between">
             <div>
@@ -12,6 +12,12 @@ const VerticalBanner =({theme, loaderVisible}:{theme:string; loaderVisible:boole
                 <div className="pt-10 font-bold text-xl">
                     <GithubIcon height={18} width={18} theme={theme}/></div>
             </div>
+            {!loaderVisible &&
+            <div className="pt-96 flex flex-col text-sm font-light">
+                <div className={`pl-20 -rotate-90 ${section==='exp'?"opacity-100":"opacity-50"} ${theme==="dark"?"text-white":"text-black"}`}>Experience</div>
+                <div className={`pr-20  -rotate-90 ${section==='proj'?"opacity-100":"opacity-50"} ${theme==="dark"?"text-white":"text-black"}`}>Projects</div>
+                <div className={`pr-56  -rotate-90 ${section==='cred'?"opacity-100":"opacity-50"} ${theme==="dark"?"text-white":"text-black"}`}>Credits</div>
+            </div>}
             <div className=""><VerticalLoader isVisible={loaderVisible}/></div>
         </div>
     )
